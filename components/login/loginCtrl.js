e5243583165482af5c32528e4fca0824115f0462
Ana Maria Ribeiro <Ana2@MacBook-Pro-de-ana.local>
2018-03-01 11:45:58 +0100

    evoluoj en ensaluta paĝo

--- components/login/loginCtrl.js
+++ components/login/loginCtrl.js
@@ -1,8 +1,8 @@
-app.controller("loginCtrl", function ($scope, $rootScope, $window, $mdDialog, $sanitize,
+app.controller("loginCtrl", function ($scope, $scope, $window, $mdDialog, $sanitize,
                                       errorService, config, loginService) {
 
   $scope.init = function() {
-    $rootScope.menuo = false;
+    $scope.menuo = false;
     $scope.msg = "ERARO: Ni ne havas konekton kun la servilo nun";
 
     // if (($window.localStorage.getItem('token') != null) &&
@@ -24,18 +24,48 @@ app.controller("loginCtrl", function ($scope, $rootScope, $window, $mdDialog, $s
         }, function(response) {
           $scope.msg = response.data.message;
       });
+  }
+
+  $scope.forgesisPas = function() {
+    var nt = $scope.uzanto.naskigxtagoSenFormo
+    $scope.uzanto.naskigxtago = (nt[4] + nt[5] + nt[6] + nt[7] + "-" +
+                                     nt[2] + nt[3] + "-" + nt[0] + nt[1]).toString();
+    var timestamp = Date.parse($scope.uzanto.naskigxtago);
+    var minDate = new Date("1877-06-26");
+    var maxDate = new Date();
+    var naskigxtago = new Date($scope.uzanto.naskigxtago);
+    if(isNaN(timestamp)) {
+       $scope.forgesis.naskigxitago.$setValidity("date", false);
+    } else {
+      if((naskigxtago < minDate) || (naskigxtago > maxDate)){
+         $scope.forgesis.naskigxitago.$setValidity("date", false);
+      } else {
+         $scope.forgesis.naskigxitago.$setValidity("date", true);
+      }
     }
 
-    $scope.montriDetalojn = function(ev, element) {
-      $mdDialog.show({
-        contentElement: element,
-        parent: angular.element(document.body),
-        targetEvent: ev,
-        clickOutsideToClose: true
+    if($scope.forgesis.$valid) {
+      loginService.forgesis($scope.uzanto).then(function(response){
+        window.alert("Nova pasvorto sendita al via retpoŝto");
+        $window.location.reload();
+        console.log(response);
+      }, function (response) {
+        $scope.err = response.data.message;
+        console.log(response);
       });
-    };
+    }
+  }
 
-    $scope.cancel = function() {
-       $mdDialog.cancel();
-    };
+  $scope.montriDetalojn = function(ev, element) {
+    $mdDialog.show({
+      contentElement: element,
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true
+    });
+  };
+
+  $scope.cancel = function() {
+     $mdDialog.cancel();
+  };
 });
--- components/login/loginService.js
+++ components/login/loginService.js
@@ -2,9 +2,14 @@ app.service('loginService', function($http, config, $window){
 	var service = this;
 
 	service.doEnsaluti = doEnsaluti;
+	service.forgesis = forgesis;
 
 	function doEnsaluti(data){
 		return $http.post(config.api_url + '/uzantoj/ensaluti', data);
+	}
+
+	function forgesis(data){
+		return $http.post(config.api_url + '/uzantoj/forgesisPasvorton', data);
 	}
 
 	return service;
